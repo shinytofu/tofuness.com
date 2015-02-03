08217c6fa43cf6b0e1089a3715b0caa25be78974
Dennis Jin <desuvader@gmail.com>
2015-02-03 09:03:42 +0100

    Started working on site design

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+
+var path = {
+	scss: {
+		src: 'src/css/style.scss',
+		files: 'src/css/**/*.scss',
+		dest: 'build/css'
+	},
+	js: {
+		src: 'src/js/main.js',
+		files: 'src/js/**/*.js',
+		dest: 'build/js'
+	}
+}
+
+gulp.task('sass', function() {
+	return gulp.src(path.scss.src)
+		.pipe(plumber(function(err) {
+			console.log(err.message);
+			this.emit('end');
+		}))
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}))
+		.pipe(gulp.dest(path.scss.dest));
+});
+
+gulp.task('uglify', function() {
+	return gulp.src(path.js.src)
+	.pipe(plumber(function(err) {
+		console.log(err.message);
+		this.emit('end');
+	}))
+	.pipe(uglify())
+	.pipe(gulp.dest(path.js.dest));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(path.scss.files, ['sass']);
+	gulp.watch(path.js.files, ['uglify']);
+});
+
+gulp.task('default', ['sass' , 'uglify', 'watch']);
